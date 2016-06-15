2f765daf88b2a3aa35016bdf86c6a41aab3568c8
bjlvsheng <326375225@qq.com>
2016-06-15 23:05:38 +0800

    RotateList.java

--- src/add/binary/MinimumSizeSubarraySum.java
+++ src/add/binary/MinimumSizeSubarraySum.java
@@ -3,9 +3,9 @@ package add.binary;
 
 /**
  * @author lvsheng
- * project LeetCodeOJ
- * date 2016年2月25日
- * time 下午7:44:12
+ *         project LeetCodeOJ
+ *         date 2016年2月25日
+ *         time 下午7:44:12
  */
 public class MinimumSizeSubarraySum {
 
@@ -13,29 +13,46 @@ public class MinimumSizeSubarraySum {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] a= {2,3,1,2,4,3};
-		System.out.print(minSubArrayLen(7, a));
+		int[] a = {2, 3, 1, 2, 4, 3};
+		int[] b = {1, 4, 4};
+		//System.out.println(minSubArrayLen(7, a));
+		System.out.println(minSubArrayLen2(4, b));
 	}
-	
+
 	public static int minSubArrayLen(int s, int[] nums) {
-        int len = nums.length;
-        if(len == 0 || s == 0) return 0;
+		int len = nums.length;
+		if (len == 0 || s == 0) return 0;
 		int[] add = new int[len];
 		add[0] = nums[0];
 		for (int i = 1; i < len; i++) {
 			add[i] += (add[i - 1] + nums[i]);
 		}
-		
+
 		int l = Integer.MAX_VALUE;
 		for (int i = 0; i < len; i++) {
 			for (int j = i + 1; j < len; j++) {
-				if(add[j] - add[i] == s && j - i < l) {
+				if (add[j] - add[i] >= s && j - i < l) {
 					l = j - i;
 				}
 			}
 		}
-		if(l == Integer.MAX_VALUE) return 0;
+		if (l == Integer.MAX_VALUE) return 0;
 		else return l;
-    }
+	}
+
+	public static int minSubArrayLen2(int s, int[] nums) {
+		int len = Integer.MAX_VALUE;
+		if (len == 0 || s == 0) return 0;
+		int left = 0, right = 0, sum = 0;
+		for (; right < nums.length; right++) {
+			sum += nums[right];
+			while (sum >= s && left <= right) {
+				len = Math.min(len, right - left + 1);
+				sum -= nums[left++];
+			}
+		}
+
+		return len == Integer.MAX_VALUE ? 0 : len;
+	}
 
 }
---
+++ src/march/RotateList.java
@@ -0,0 +1,48 @@
+package march;
+
+/**
+ * Created by Administrator on 2016/6/15.
+ */
+public class RotateList {
+	public static void main(String[] args) {
+		ListNode h = new ListNode(1);
+		h.next = null;
+		rotateRight(h, 1);
+	}
+
+	public static ListNode rotateRight(ListNode head, int k) {
+		if (head == null) return null;
+
+		ListNode h = new ListNode(0);
+		h.next = head;
+		ListNode p1 = h, p2 = h, p = h;
+
+		int len = 0;
+		while (p.next != null) {
+			p = p.next;
+			len++;
+		}
+		k %= len;
+		if (k == 0) return head;
+
+		for (int i = 0; i < k; i++) p2 = p2.next;
+		while (p2.next != null) {
+			p1 = p1.next;
+			p2 = p2.next;
+		}
+
+		h.next = p1.next;
+		p2.next = head;
+		p1.next = null;
+		return h.next;
+	}
+}
+
+class ListNode {
+	int      val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
