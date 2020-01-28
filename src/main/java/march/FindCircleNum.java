d6c07dd0a56f7b7886a6ff1daa786c25d419404c
LvSheng <326375225@qq.com>
2020-01-28 12:16:47 +0800

    并查集

---
+++ src/main/java/march/FindCircleNum.java
@@ -0,0 +1,39 @@
+package march;
+
+/**
+ * @author LvSheng
+ * @date 2020/1/28
+ **/
+public class FindCircleNum {
+	
+	public int findCircleNum(int[][] M) {
+		int[] parents = new int[M.length];
+		for (int i = 0; i < parents.length; i++) parents[i] = i;
+		for (int i = 0; i < M.length; i++)
+			for (int j = i + 1; j < M.length; j++) {
+				if (i == j) continue;
+				if (M[i][j] == 1) {
+					int p1 = getParents(parents, i);
+					int p2 = getParents(parents, j);
+					if (p1 != p2) {
+						parents[p1] = p2;
+					}
+				}
+			}
+		
+		int sum = 0;
+		
+		for (int i = 0; i < M.length; i++) {
+			if (parents[i] == i) sum++;
+		}
+		
+		return sum;
+	}
+	
+	private int getParents(int[] p, int i) {
+		while (p[i] != i) {
+			i = p[i];
+		}
+		return i;
+	}
+}
