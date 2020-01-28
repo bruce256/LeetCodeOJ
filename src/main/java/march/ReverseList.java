c9e02c9f1e7a25ed6f572c2ddd94f8e3041f84e8
LvSheng <326375225@qq.com>
2020-01-28 10:14:52 +0800

    反转链表

---
+++ src/main/java/march/ReverseList.java
@@ -0,0 +1,43 @@
+package march;
+
+import java.util.List;
+
+/**
+ * @author LvSheng
+ * @date 2020/1/28
+ **/
+public class ReverseList {
+	
+	public ListNode reverseList(ListNode head) {
+		if (head == null || head.next == null) return head;
+		
+		ListNode p1 = head.next;
+		ListNode p2 = p1.next;
+		head.next = null;
+		while (p2 != null) {
+			p1.next = head;
+			head = p1;
+			
+			p1 = p2;
+			p2 = p2.next;
+		}
+		
+		p1.next = head;
+		return p1;
+	}
+	
+	public static void main(String[] args) {
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(2);
+		ListNode l3 = new ListNode(3);
+		ListNode l4 = new ListNode(4);
+		ListNode l5 = new ListNode(5);
+		l1.next = l2;
+		l2.next = l3;
+		l3.next = l4;
+		l4.next = l5;
+		
+		ReverseList reverseList = new ReverseList();
+		reverseList.reverseList(l1);
+	}
+}
