2ee4be549ddb04431da6c886fb69aa36b3c2f7b7
LvSheng <326375225@qq.com>
2020-02-13 22:12:35 +0800

    完全平方数

--- src/main/java/backtrack/NumSquares.java
+++ src/main/java/backtrack/NumSquares.java
@@ -1,9 +1,13 @@
 package backtrack;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
 
 /**
+ * bfs 解决
+ *
  * @author LvSheng
  * @date 2020/2/6
  **/
@@ -12,6 +16,7 @@ public class NumSquares {
 	
 	public int numSquares(int n) {
 		Queue<Integer> queue = new LinkedList<>();
+		Set<Integer>   set   = new HashSet<>();
 		queue.add(n);
 		int num = 0;
 		while (!queue.isEmpty()) {
@@ -22,7 +27,10 @@ public class NumSquares {
 				for (int j = 1; cur - j * j >= 0; j++) {
 					int c = cur - j * j;
 					if (c == 0) return num;
-					else if (c > 0) queue.add(c);
+					else if (c > 0 && !set.contains(c)) {
+						queue.add(c);
+						set.add(c);
+					}
 				}
 			}
 		}
