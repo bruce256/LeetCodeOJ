05ed68682ebed4088f1ddb124b9fa465680d45d5
lvsheng1 <326375225@qq.com>
2023-10-26 22:41:56 +0800

    merge

---
+++ src/main/java/MergeKLists.java
@@ -0,0 +1,19 @@
+
+/**
+ * @author 吕胜 lvheng1
+ * @date 2022/12/19
+ **/
+public class MergeKLists {
+	
+	public ListNode mergeKLists(ListNode[] lists) {
+		return null;
+	}
+}
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
