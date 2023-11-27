3195311f88d0821681c5170f185da33a241bbd95
lvsheng1 <326375225@qq.com>
2023-11-27 22:49:13 +0800

    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/

--- src/main/java/add/binary/TwoSum.java
+++ src/main/java/add/binary/TwoSum.java
@@ -1,41 +1,43 @@
 package add.binary;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 
 /**
+ * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+ *
  * @author lvsheng
  * project LeetCodeOJ
  * date 2016年2月18日
  * time 下午5:00:33
  */
 public class TwoSum {
-
+	
 	public static void main(String[] args) {
-		int[] a = { 2, 7, 11, 15 };
-		System.out.println(twoSum(a, 9));
+		int[] a      = {2, 7, 11, 15};
+		int[] result = twoSum(a, 9);
+		System.out.println(result[0] + "\t" + result[1]);
 	}
 	
-	public static int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        int n2;
-        Integer idx;
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>(nums.length);
-        
-        for (int i = 0; i < nums.length; i++) {
-			map.put(nums[i], i);
+	public static int[] twoSum(int[] numbers, int target) {
+		int[]                 result = new int[2];
+		int                   n2;
+		Integer               idx;
+		Map<Integer, Integer> map    = new HashMap<Integer, Integer>(numbers.length);
+		
+		for (int i = 0; i < numbers.length; i++) {
+			map.put(numbers[i], i);
 		}
-        for (int i = 0; i < nums.length - 1; i++) {
-			n2 = target - nums[i];
+		for (int i = 0; i < numbers.length - 1; i++) {
+			n2  = target - numbers[i];
 			idx = map.get(n2);
-			if(idx != null && idx > i){
-				result[0] = i;
-				result[1] = idx;
+			if (idx != null && idx > i) {
+				result[0] = i + 1;
+				result[1] = idx + 1;
 				break;
 			}
 		}
-        return result;
-    }
+		return result;
+	}
 }
