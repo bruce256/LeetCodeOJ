4407135f28e38f5c0449b91cced19a9cab06016a
“bruce256” <326375225@qq.com>
2024-03-30 13:10:53 +0800

    穷举求解

--- src/main/java/backtrack/Subsets.java
+++ src/main/java/backtrack/Subsets.java
@@ -7,26 +7,30 @@ import java.util.Collections;
 import java.util.List;
 
 /**
+ * https://leetcode.cn/problems/subsets/description/
+ *
  * @author lvsheng
- *         project LeetCodeOJ
- *         date 2016年2月16日
- *         time 下午8:44:06
+ * project LeetCodeOJ
+ * date 2016年2月16日
+ * time 下午8:44:06
  */
 public class Subsets {
-
+	
 	public static void main(String[] args) {
-		int[] a = { 1, 2, 3 };
-		System.out.println(subsets(a));
+		Subsets test = new Subsets();
+		int[]   a    = {1, 2, 3};
+		System.out.println(test.subsets1(a));
+		System.out.println(test.subsets(a));
 	}
-
-	public static List<List<Integer>> subsets(int[] nums) {
+	
+	public List<List<Integer>> subsets(int[] nums) {
 		List<List<Integer>> ll = new ArrayList<List<Integer>>();
 		Arrays.sort(nums);
 		dfs(nums, new ArrayList<Integer>(), 0, ll);
 		return ll;
 	}
-
-	private static void dfs(int[] nums, ArrayList<Integer> l, int i, List<List<Integer>> ll) {
+	
+	private void dfs(int[] nums, ArrayList<Integer> l, int i, List<List<Integer>> ll) {
 		if (i == nums.length) {
 			ll.add((List<Integer>) l.clone());
 			return;
@@ -36,4 +40,20 @@ public class Subsets {
 		l.remove(l.size() - 1);
 		dfs(nums, l, i + 1, ll);
 	}
+	
+	// 掩码的方法求解
+	public List<List<Integer>> subsets1(int[] nums) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		for (int mask = 0; mask < (1 << nums.length); mask++) {
+			ArrayList<Integer> list = new ArrayList<>();
+			for (int j = 0; j < nums.length; j++) {
+				if ((mask & (1 << j)) > 0) {
+					list.add(nums[j]);
+				}
+			}
+			result.add(list);
+		}
+		return result;
+	}
+	
 }
--- src/main/java/easy/Subsets.java
+++ src/main/java/easy/Subsets.java
@@ -14,18 +14,27 @@ public class Subsets {
 	
 	public List<List<Integer>> subsets(int[] nums) {
 		result = new ArrayList<>();
+		dfs(0, nums, new ArrayList<>());
 		return result;
 	}
 	
 	private void dfs(int level, int[] nums, List<Integer> list) {
 		if (level == nums.length) {
-			ArrayList<Integer> dest = new ArrayList<>();
-			Collections.copy(dest, list);
+			List<Integer> dest = new ArrayList<>(list.size());
+			dest.addAll(list);
 			result.add(dest);
 			return;
 		}
+		
 		list.add(nums[level]);
 		dfs(level + 1, nums, list);
 		
+		list.remove(list.size() - 1);
+		dfs(level + 1, nums, list);
+	}
+	
+	public static void main(String[] args) {
+		Subsets test = new Subsets();
+		System.out.println(test.subsets(new int[]{1, 2, 3}));
 	}
 }
