1607d8d7a1cb4c7b4a9c3ae006920068e451a38e
“bruce256” <326375225@qq.com>
2024-05-05 22:17:37 +0800

    单个函数解决问题

--- src/main/java/tree/IsSameTree.java
+++ src/main/java/tree/IsSameTree.java
@@ -10,7 +10,25 @@ public class IsSameTree {
 	
 	private boolean result;
 	
+	/**
+	 * 单个函数解决问题
+	 *
+	 * @param p
+	 * @param q
+	 * @return
+	 */
 	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		}
+		if ((p == null || q == null) || p.val != q.val) {
+			return false;
+		}
+		
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	}
+	
+	public boolean isSameTree1(TreeNode p, TreeNode q) {
 		result = true;
 		dfs(p, q);
 		return result;
