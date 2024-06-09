640a855fd9b1068357de7af83cfdbb3bb5c88780
“bruce256” <326375225@qq.com>
2024-06-09 12:31:38 +0800

    固定窗口时间算法

--- src/main/java/slidingwindow/MinSubArrayLen.java
+++ src/main/java/slidingwindow/MinSubArrayLen.java
@@ -8,7 +8,7 @@ package slidingwindow;
  **/
 public class MinSubArrayLen {
 	
-	public int minSubArrayLen(int target, int[] nums) {
+	public int minSubArrayLen1(int target, int[] nums) {
 		int left = 0, right = 0, minLen = Integer.MAX_VALUE;
 		int sum  = 0;
 		while (right < nums.length) {
@@ -21,5 +21,26 @@ public class MinSubArrayLen {
 			}
 		}
 		return minLen == Integer.MAX_VALUE ? 0 : minLen;
+	}
+	
+	public int minSubArrayLen(int target, int[] nums) {
+		int left = 0, right = 0, minLen = Integer.MAX_VALUE;
+		int sum  = 0;
+		while (right < nums.length) {
+			if (sum < target) {
+				sum += nums[right];
+				right++;
+			} else {
+				minLen = Math.min(minLen, right - left);
+				sum -= nums[left];
+				left++;
+			}
+		}
+		return minLen == Integer.MAX_VALUE ? 0 : minLen;
+	}
+	
+	public static void main(String[] args) {
+		MinSubArrayLen test = new MinSubArrayLen();
+		System.out.println(test.minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3}));
 	}
 }
