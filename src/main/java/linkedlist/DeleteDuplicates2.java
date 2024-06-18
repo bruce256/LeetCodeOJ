916d3aa6508b0d5c49618710134b9c6e9107f8cd
“bruce256” <326375225@qq.com>
2024-06-18 23:03:05 +0800

    https://leetcode.cn/problems/remove-duplicates-from-sorted-list-ii/

--- src/main/java/linkedlist/DeleteDuplicates2.java
+++ src/main/java/linkedlist/DeleteDuplicates2.java
@@ -9,16 +9,33 @@ package linkedlist;
 public class DeleteDuplicates2 {
 	
 	public ListNode deleteDuplicates(ListNode head) {
+		if (head == null) {
+			return head;
+		}
+		
 		ListNode dummy = new ListNode(0, head);
-		ListNode left  = dummy;
-		ListNode right = dummy.next;
+		ListNode pre   = dummy;
+		ListNode left  = head;
+		ListNode right = head.next;
+		
 		while (right != null) {
-			while (right.next != null && left.next.val == right.next.val) {
+			while (right != null && left.val == right.val) {
 				right = right.next;
 			}
 			
-			left.next = right.next;
+			if (left.next != right) {
+				pre.next = right;
+				left     = right;
+				if (right != null) {
+					right = right.next;
+				}
+			} else {
+				pre   = left;
+				left  = right;
+				right = right.next;
+			}
 		}
-		return null;
+		
+		return dummy.next;
 	}
 }
