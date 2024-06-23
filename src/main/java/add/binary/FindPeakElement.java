09ed93e358b36c165730c790b6ffdc40783dc02d
“bruce256” <326375225@qq.com>
2024-06-23 15:48:02 +0800

    暴力求解

--- src/main/java/add/binary/FindPeakElement.java
+++ src/main/java/add/binary/FindPeakElement.java
@@ -2,19 +2,33 @@ package add.binary;
 
 
 /**
+ * https://leetcode.cn/problems/find-peak-element/description
+ *
  * @author lvsheng
  * project LeetCodeOJ
  * date 2016年2月16日
  * time 下午8:37:41
  */
 public class FindPeakElement {
-
+	
+	public int findPeakElement(int[] nums) {
+		int idx = 0;
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] > nums[idx]) {
+				idx = i;
+			} else {
+				break;
+			}
+		}
+		
+		return idx;
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
 	}
-
+	
 }
