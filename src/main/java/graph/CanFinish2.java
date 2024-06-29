208dd4bd2d5cacebf4813a2068a1edd59376c8d0
“bruce256” <326375225@qq.com>
2024-06-29 10:50:25 +0800

    2016年版本

---
+++ src/main/java/graph/CanFinish2.java
@@ -0,0 +1,56 @@
+package graph;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * @auther: LvSheng
+ * @date: 2024/6/29
+ * @description:
+ */
+public class CanFinish2 {
+	public boolean canFinish(int numCourses, int[][] prerequisites) {
+		Edge[]    edges   = new Edge[numCourses];
+		int[]     indgree = new int[numCourses];
+		boolean[] visited = new boolean[numCourses];
+		for (int i = 0; i < numCourses; i++) edges[i] = new Edge(-1);
+		
+		for (int i = 0; i < prerequisites.length; i++) {
+			int[] edge = prerequisites[i];
+			Edge e = new Edge(edge[1]);
+			int edgeHead = edge[0];
+			e.next = edges[edgeHead].next;
+			edges[edgeHead].next = e;
+			
+			indgree[edge[1]]++;
+		}
+		Queue<Integer> q = new ArrayDeque<Integer>(numCourses);
+		for (int i = 0; i < numCourses; i++) {
+			if (indgree[i] == 0) q.add(i);
+		}
+		if (q.isEmpty()) return false;
+		
+		while (!q.isEmpty()) {
+			int ver = q.poll();
+			visited[ver] = true;
+			Edge head = edges[ver].next;
+			while (head != null) {
+				if (--indgree[head.num] == 0) q.add(head.num);
+				head = head.next;
+			}
+		}
+		
+		boolean result = true;
+		for (boolean v : visited) result &= v;
+		return result;
+	}
+}
+
+class Edge {
+	public Edge(int n) {
+		num = n;
+	}
+	
+	public int num;
+	public Edge next = null;
+}
