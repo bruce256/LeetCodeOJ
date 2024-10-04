aa197c804d2ad474acf6aa516eabd72d8db33529
“bruce256” <326375225@qq.com>
2024-10-04 21:36:01 +0800

    dfs求解

--- src/main/java/tree/RightSideView.java
+++ src/main/java/tree/RightSideView.java
@@ -13,7 +13,13 @@ import java.util.Queue;
  **/
 public class RightSideView {
 	
-	public List<Integer> rightSideView(TreeNode root) {
+	/**
+	 * bfs求解
+	 *
+	 * @param root
+	 * @return
+	 */
+	public List<Integer> rightSideView1(TreeNode root) {
 		List<Integer> list = new ArrayList<>();
 		if (root == null) {
 			return list;
@@ -32,5 +38,30 @@ public class RightSideView {
 			list.add(last.val);
 		}
 		return list;
+	}
+	
+	/**
+	 * 深搜求解
+	 *
+	 * @param root
+	 * @return
+	 */
+	public List<Integer> rightSideView(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		dfs(root, 0, result);
+		return result;
+	}
+	
+	private void dfs(TreeNode root, int depth, List<Integer> result) {
+		if (root == null) {
+			return;
+		}
+		
+		if (depth >= result.size()) {
+			result.add(root.val);
+		}
+		
+		dfs(root.right, depth + 1, result);
+		dfs(root.left, depth + 1, result);
 	}
 }
