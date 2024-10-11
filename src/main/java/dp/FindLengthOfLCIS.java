1fdc58dcb1c49c72ecdf7d90a2bb32702c13137d
“bruce256” <326375225@qq.com>
2024-10-11 23:07:39 +0800

    暴力求解

--- src/main/java/dp/FindLengthOfLCIS.java
+++ src/main/java/dp/FindLengthOfLCIS.java
@@ -3,6 +3,8 @@ package dp;
 import java.util.Arrays;
 
 /**
+ * https://leetcode.cn/problems/longest-continuous-increasing-subsequence/description/
+ *
  * @author LvSheng
  * @date 2020/2/4
  **/
@@ -11,18 +13,16 @@ public class FindLengthOfLCIS {
 	public int findLengthOfLCIS(int[] nums) {
 		if (nums.length == 0) return 0;
 		
-		int[] dp = new int[nums.length];
-		Arrays.fill(dp, 1);
-		int anchor = 0;
-		int result = 1;
-		for (int i = 1; i < nums.length; i++) {
-			if (nums[i] > nums[i - 1]) {
-				dp[i] = dp[i - 1] + 1;
-				result = Math.max(result, i - anchor + 1);
-			} else anchor = i;
-			
+		int max = 0;
+		int left = 0;
+		for (int right = 0; right < nums.length; right++) {
+			// 非递增的时候更新窗口左边的值
+			if (right > 0 && nums[right] <= nums[right - 1]) {
+				left = right;
+			}
+			// 及时更新最大值
+			max = Math.max(max, right - left + 1);
 		}
-		
-		return result;
+		return max;
 	}
 }
